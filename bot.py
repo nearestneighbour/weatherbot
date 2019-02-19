c6e38ae3aba4441c27a3f0bb1018616b2b70c752
nearestneighbour <tido_berg@hotmail.com>
2019-02-19 18:08:09 +0100

    text

--- bot.py
+++ bot.py
@@ -2,41 +2,39 @@ from flask import Flask, request
 import requests
 
 app = Flask(__name__)
-weathermsg = 'Temperatuur: {} C\nWind: {} m/s\nRegenkans: {}%'
+mychat = 571267260
+API_KEY = '694985196:AAFoZWhhsDF4qyvj0bJQNjn89PZypztg3Xc'
+BOT_URL = 'https://api.telegram.org/bot' + API_KEY + '/'
+weathertxt = 'Temperatuur: {} C\nWind: {} m/s\nRegenkans: {}%'
 
-def send_message(prepared_data):
-    message_url = 'https://api.telegram.org/bot694985196:AAFoZWhhsDF4qyvj0bJQNjn89PZypztg3Xc/sendMessage'
-    requests.post(message_url, json=prepared_data)
+def send_message(chat_id, text):
+    url = BOT_URL + 'sendMessage'
+    data = {'chat_id':chat_id, 'text':text}
+    requests.post(message_url, json=data)
 
-def getweather(location):
-    url = 'https://weerlive.nl/api/json-10min.php?locatie='+location
+def getweather(loc):
+    if isinstance(loc, dict):
+        loc = str(loc['latitude'])+','+str(loc['longitude'])
+    url = 'https://weerlive.nl/api/json-10min.php?locatie=' + loc
     data = requests.get(url,timeout=10).json()['liveweer'][0]
-    return data
+    response = weathermsg.format(data['temp'],data['windms'],data['d0neerslag'])
+    return response
+
 
 @app.route('/hello')
 def hello_world():
     return 'Hello, World!12'
 
-@app.route('/txt', methods=['GET','POST'])
+@app.route('/txt', methods=['POST'])
 def getmsg():
-    if request.method == 'GET':
-        if 'chat_id' not in request.args:
-            return "empty get request"
-        chat_id = request.args['chat_id']
-        txt = request.args['text']
-        vv = 'get'
-    elif request.method == 'POST':
-        data = request.get_json()
-        if 'message' in data:
-            msg = data['message']
-            chat_id = msg['chat']['id']
-            if 'location' in msg:
-                location = str(msg['location']['latitude'])+','+str(msg['location']['longitude'])
-                weather = getweather(location)
-                txt = weathermsg.format(weather['temp'],weather['windms'],weather['d0neerslag'])
-                vv = 'loc'
-            elif 'text' in msg:
-                txt = msg['text'][::-1]
-                vv = 'rev'
-    send_message({'chat_id':chat_id,'text':txt})
-    return vv
+    data = request.get_json()
+    if 'message' in data:
+        data = data['message']
+        chat = data['chat']['id']
+        if 'location' in data:
+            text = getweather(data['location'])
+        elif 'text' in data:
+            text = getweather(data['text'])
+
+    send_message(chat,text)
+    return ''
