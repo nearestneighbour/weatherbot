8abf6a9ccab538bc53dc1c19ff3e456beeb5d98c
nearestneighbour <tido_berg@hotmail.com>
2019-02-19 17:41:09 +0100

    test

--- bot.py
+++ bot.py
@@ -2,11 +2,16 @@ from flask import Flask, request
 import requests
 
 app = Flask(__name__)
-BOT_URL = 'https://api.telegram.org/bot694985196:AAFoZWhhsDF4qyvj0bJQNjn89PZypztg3Xc/'
+weathermsg = 'Temperatuur: {} C\nWind: {} m/s\nRegenkans: {}%'
 
 def send_message(prepared_data):
-    message_url = BOT_URL + 'sendMessage'
+    message_url = 'https://api.telegram.org/bot694985196:AAFoZWhhsDF4qyvj0bJQNjn89PZypztg3Xc/sendMessage'
     requests.post(message_url, json=prepared_data)
+
+def getweather(location):
+    url = 'https://weerlive.nl/api/json-10min.php?locatie='+location
+    data = requests.get(url,timeout=10).json()['liveweer'][0]
+    return data
 
 @app.route('/hello')
 def hello_world():
@@ -15,13 +20,23 @@ def hello_world():
 @app.route('/txt', methods=['GET','POST'])
 def getmsg():
     if request.method == 'GET':
-        chid = request.args['chat_id']
-        msg = request.args['text']
+        if 'chat_id' not in request.args:
+            return "empty get request"
+        chat_id = request.args['chat_id']
+        txt = request.args['text']
+        vv = 'get'
     elif request.method == 'POST':
         data = request.get_json()
-        chid = data['message']['chat']['id']
-        msg = data['message']['text']
-    send_message({'chat_id':chid,'text':msg})
-    return "test"
-
-#app.run()
+        if 'message' in data:
+            msg = data['message']
+            chat_id = msg['chat']['id']
+            if 'location' in msg:
+                location = str(msg['location']['latitude'])+','+str(msg['location']['longitude'])
+                weather = getweather(location)
+                txt = weathermsg.format(weather['temp'],weather['windms'],weather['d0neerslag'])
+                vv = 'loc'
+            elif 'text' in msg:
+                txt = msg['text'][::-1]
+                vv = 'rev'
+    send_message({'chat_id':chat_id,'text':txt})
+    return vv
